84fb955041f67aed1181a4fbfed5f2d63678ed0c
SkandTiwari <tiwariskand11@gmail.com>
2022-03-24 13:06:24 +0530

    Beta version ready, Login, main menu, encyption, decryption windows ready

---
+++ src/Endec.java
@@ -0,0 +1,19 @@
+/*import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.lang.*;*/
+import java.sql.*;
+import java.util.*;
+public class Endec {
+   
+    
+
+    public static void main(String[] args) {
+       
+        new LoginWindow().setVisible(true);
+        System.out.println("Welcome to CiphrX");
+    }
+}
+
+
+
